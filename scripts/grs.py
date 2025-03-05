5dbca30eb45f5e3c1ac9ab68657b7f306c924f10
Maurizio <maurizio.mazza@edu.unifi.it>
2025-03-05 15:20:51 +0100

    Refactor GRS algorithm to support morning and afternoon shifts with updated operator state management

--- scripts/grs.py
+++ scripts/grs.py
@@ -1,4 +1,4 @@
-# Script utilizzato per implementare l'algoritmo 1 GRS
+# Script utilizzato per implementare l'algoritmo 1 GRS - Time
 
 import datetime, math
 from operators_requests import Operator, Request, Node
@@ -14,17 +14,32 @@ def sort_requests_by_alpha(requests):
     """
     return sorted(requests, key=lambda r: r.temporal_window[0])
 
+# funzione per impostare lo stato di operatore per il turno mattutino (eo = 420)
+def set_operator_state_morning(operator):
+    """
+    Reimposta lo stato dell'operatore per il turno mattutino.
+    
+    :param operator: Oggetto di tipo Operator.
+    """
+    operator.current_patient = operator.home
+    operator.eo = 420 # Inizio turno mattutino: 7:00
+    operator.ho = 300 # Tempo residuo disponibile: 5 ore
+    operator.wo = 0 # tempo di lavoro accumulato
+    operator.Lo = [] # Lista di richieste assegnate, inizialmente vuota
 
-## funzione per reimpostare lo stato di operatore per il turno pomeridiano (t0 = 960)
-def reset_operator_state_afternoon(operator):
+
+## funzione per impostare lo stato di operatore per il turno pomeridiano (eo = 960)
+def set_operator_state_afternoon(operator):
     """
     Reimposta lo stato dell'operatore per il turno pomeridiano.
     
     :param operator: Oggetto di tipo Operator.
     """
     operator.current_patient = operator.home
-    operator.t0 = 960
-    operator.work_time = 300
+    operator.eo = 960 # Inizio turno pomerdiano: 16:00
+    operator.ho = 300 # Tempo residuo disponibile: 5 ore
+    operator.wo = 0 # tempo di lavoro accumulato
+    operator.Lo = [] # Lista di richieste assegnate, inizialmente vuota
 
 # funzione per calcolare il tempo di viaggio tra due nodi
 def compute_travel_time(node_a: Node, node_b: Node) -> float:
@@ -37,32 +52,44 @@ def compute_travel_time(node_a: Node, node_b: Node) -> float:
     return math.sqrt(dx**2 + dy**2)
 
 # funzione per verificare la fattibilità di una richiesta per un operatore
-def is_feasible(operator: Operator, request: Request) -> bool:
+def is_feasible(operator: Operator, request: Request, shift_end: int, debug: bool) -> Tuple[bool, Optional[str]]:
     """
     Verifica la fattibilità della richiesta per un operatore.
     Condizione: 
-      αᵢ ≤ tₒ + τₚₒᵢ ≤ βᵢ  e  dᵢ + τₚₒᵢ ≤ wₒ
+      eₒ + τₚₒₚᵢ ≤ βᵢ  e  dᵢ ≤ hₒ
+    Restituisce:
+      - (True, None) se fattibile
+      - (False, "arrival") se il problema è l'orario di arrivo
+      - (False, "work") se il problema è il tempo di lavoro residuo  
     """
+    
     travel_time = compute_travel_time(operator.current_patient, request.patient)
-    arrival_time = operator.t0 + travel_time
+    arrival_time = operator.eo + travel_time
     alpha, beta = request.temporal_window
 
-    # Controllo finestra temporale
-    if not (arrival_time <= beta):
-        print(f"Richiesta {request.i} NON fattibile per l'operatore {operator.id}:")
-        print(f"  Arrival time = {arrival_time:.2f} non rientra in finestra ({alpha}, {beta})")
-        return False
-
-    # Controllo tempo di lavoro residuo
-    if request.duration > operator.work_time:
-        print(f"Richiesta {request.i} NON fattibile per l'operatore {operator.id}:")
-        print(f"        Durata = {request.duration} > Tempo rimanente w0 = {operator.work_time:.0f}")
-        return False
-
-    return True
+    
+    if arrival_time > beta:
+        if debug:
+            print(f"Richiesta {request.i} rifiutata per l'operatore {operator.id}:")
+            print(f"  Arrival time = {arrival_time:.2f} non rientra in finestra ({alpha}, {beta})")
+        return False, "arrival"
+    
+    if operator.eo + travel_time + request.duration > shift_end:
+        if debug:
+            print(f"Richiesta {request.i} rifiutata per l'operatore {operator.id}:")
+            print(f"  supererebbe la fine del turno (eo + travel + duration = {operator.eo + travel_time + request.duration:.2f} > {shift_end})")
+        return False, "work"
+    
+    if travel_time + request.duration > operator.ho:
+        if debug:
+            print(f"Richiesta {request.i} rifiutata per l'operatore {operator.id}:")
+            print(f"  (Travel time + Durata) = {travel_time + request.duration:.2f} > Tempo rimanente ho = {operator.ho:.0f}")
+        return False, "work"
+    
+    return True, None
 
 # funzione per selezionare il miglior operatore per una richiesta
-def select_best_operator_for_request(request: Request, operators: List[Operator]) -> Optional[Operator]:
+def select_best_operator_for_request(request: Request, operators: List[Operator], shift_end: int) -> Optional[Operator]:
     """
     Seleziona l'operatore che minimizza il travel time (τₚₒᵢ) tra quelli
     appartenenti al cluster della richiesta. Se nessun operatore è fattibile,
@@ -74,7 +101,7 @@ def select_best_operator_for_request(request: Request, operators: List[Operator]
     feasible_ops = []
     for op in relevant_ops:
         travel_time = compute_travel_time(op.current_patient, request.patient)
-        if is_feasible(op, request):
+        if is_feasible(op, request, shift_end, debug=False)[0]:
             feasible_ops.append((op, travel_time))
     
     if not feasible_ops:
@@ -85,41 +112,109 @@ def select_best_operator_for_request(request: Request, operators: List[Operator]
 
 
 # funzione per eseguire il GRS
-def grs(operators: List[Operator],
-        requests: List[Request]) -> dict:
+def grs_time(operators: List[Operator],
+             requests: List[Request],
+             is_morning: bool = True) -> Tuple[dict[int, List[Request]], dict[str, float]]:
     """
     Greedy Routing and Scheduling (GRS).
     1. Ordina le richieste per αᵢ
     2. Per ciascuna richiesta, trova l'operatore fattibile con min travel_time
-    3. Aggiorna lo stato dell'operatore (wₒ, tₒ, pₒ)
-    4. Ritorna uno schedule sotto forma di dict: {op_id: [richieste]}
+    3. Aggiorna lo stato dell'operatore (ho, eo, wo, current_patient e Lo)
+    4. Ritorna uno schedule e le statistiche del processo di scheduling
+
+    Il parametro is_morning controlla il turno da simulare:
+      - True: turno mattutino (shift_end = 720)
+      - False: turno pomeridiano (shift_end = 1290, con reset dello stato)
+
+    Restituisce una tupla con due elementi:
+        - Uno schedule sotto forma di dict: {op_id: [richieste]}
+        - Un dizionario con le statistiche del processo di scheduling
+
+    Le statistiche includono:
+      - total_requests: numero totale di richieste elaborate
+      - assigned: numero di richieste assegnate
+      - not_assigned: numero di richieste non assegnate
+      - arrival_fail: numero di richieste non assegnate per arrival time non rispettato
+      - work_fail: numero di richieste non assegnate per work time non rispettato
     """
+
+     # Imposta shift_end in base al turno
+    if is_morning:
+        shift_end = 720  # turno mattutino
+        for op in operators:
+            set_operator_state_morning(op)
+    else:
+        shift_end = 1290  # turno pomeridiano
+        # set dello stato per il turno pomeridiano
+        for op in operators:
+            set_operator_state_afternoon(op)
+
     # (1) Ordina le richieste per αᵢ
     sorted_requests = sorted(requests, key=lambda r: r.temporal_window[0])
 
     # (2) Inizializza uno schedule vuoto per ciascun operatore
     schedule = {op.id: [] for op in operators}
+    stats = {
+        'total_requests': len(sorted_requests),
+        'assigned': 0,
+        'not_assigned': 0,
+        'arrival_fail': 0, # richieste non assegnate per arrival time non rispettato
+        'work_fail': 0 # richieste non assegnate per work time non rispettato
+    }
 
     # (3) Per ciascuna richiesta, trova l'operatore con min travel_time
     for req in sorted_requests:
         # Seleziona il miglior operatore per la richiesta
-        chosen_op = select_best_operator_for_request(req, operators)
+        chosen_op = select_best_operator_for_request(req, operators, shift_end)
+        
+        # Se non è possibile assegnare la richiesta, imposta chosen_op a None
+        if chosen_op is not None and chosen_op.ho <= 0:
+            chosen_op = None
         
         if chosen_op is not None:
             # Calcola il tempo di viaggio tra il paziente attuale dell'operatore e il paziente della richiesta
             travel_time = compute_travel_time(chosen_op.current_patient, req.patient)
 
-            # Aggiorna lo stato dell'operatore
-            chosen_op.work_time -= (travel_time + req.duration)
-            chosen_op.t0 += (travel_time + req.duration)
-            chosen_op.current_patient = req.patient
+            # Calcola l'istante di inizio servizio: s = max(eo + travel, αᵢ)
+            s = max(chosen_op.eo + travel_time, req.temporal_window[0])
 
+            # Aggiorna la lista delle richieste assegnate (Lo)
+            # Lo tiene traccia delle assegnazioni come tuple: (richiesta, istante di inizio servizio)
+            chosen_op.Lo.append((req, s))
+
+            # Aggiorna il tempo di lavoro accumulato (wo)
+            chosen_op.wo += travel_time + req.duration
+
+            # Aggiorna il primo istante disponibile per servire la prossima richiesta (eo)
+            chosen_op.eo = s + req.duration
+
+            # Aggiorna il tempo residuo del turno (ho): shift_end - eo
+            chosen_op.ho = shift_end - chosen_op.eo
+
+            # Aggiorna il current patient: l'operatore si sposta presso il paziente della richiesta
+            chosen_op.current_patient = req.patient
+            
             # Aggiunge la richiesta allo schedule dell'operatore
             schedule[chosen_op.id].append(req)
+            stats['assigned'] += 1
         else:
-            print(f"Richiesta {req.i} non assegnabile a nessun operatore.")
+            # La richiesta non è assegnabile: aggiorna il contatore
+            stats['not_assigned'] += 1
+            # Per capire il motivo, esamina gli operatori rilevanti
+            reasons = set()
+            relevant_ops = filter_operators_by_cluster(req, operators)
+            for op in relevant_ops:
+                feasible, reason = is_feasible(op, req, shift_end, debug=True)
+                if not feasible and reason is not None:
+                    reasons.add(reason)
+            # Se la ragione è "arrival", incrementa arrival_fail; altrimenti se "work", incrementa work_fail.
+            if "arrival" in reasons:
+                stats['arrival_fail'] += 1
+            elif "work" in reasons:
+                stats['work_fail'] += 1
+            # Se non è possibile determinarla, non si aggiornano ulteriori contatori
 
-    return schedule
+    return schedule, stats
 
 
 # funzione per filtrare gli operatori per cluster
