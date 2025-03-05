5c55e8c85782841f69d3aed6d35fddfb3b10ea59
Maurizio <maurizio.mazza@edu.unifi.it>
2025-03-05 15:21:04 +0100

    Refactor Operator class constructor to improve parameter naming and add request list

--- scripts/operators_requests.py
+++ scripts/operators_requests.py
@@ -1,6 +1,6 @@
 # Funzioni e classi per la gestione degli operatori e delle richieste
 
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List, Any
 
 class Node:
     def __init__(self, id: int, coordinates: Tuple[float, float]):
@@ -18,29 +18,41 @@ class Node:
 
 
 class Operator:
-    def __init__(self, id: int, home: Node, work_time: int = 300, current_patient: Optional[Node] = None, t0: int = 420, cluster_id: Optional[int] = None):
+    def __init__(self, 
+                 id: int, 
+                 home: Node, 
+                 ho: int = 300, 
+                 current_patient: Optional[Node] = None, 
+                 eo: int = 420, 
+                 cluster_id: Optional[int] = None, 
+                 wo: int = 0, 
+                 Lo: Optional[List[Tuple[Any, Any]]] = None):
         """
         Classe Operatore
 
         :param id: Id dell'operatore.
         :param home: Nodo di partenza (depot) dell'operatore, di tipo Node.
-        :param work_time: Tempo di lavoro disponibile per l'operatore per effettuare visite.
+        :param ho: il tempo rimanente fino alla fine della sessione per l'operatore o ∈ O
         :param current_patient: Il paziente attuale assegnato all'operatore, rappresentato come Node.
-        :param t0: Il primo momento disponibile per l'operatore per servire una richiesta, 420 = morning shift, 960 = afternoon shift.
+        :param eo: Il primo momento disponibile per l'operatore per servire una richiesta, 420 = morning shift, 960 = afternoon shift.
         :param cluster_id: Identificativo del cluster a cui appartiene l'operatore.
+        :param wo: è il tempo di lavoro dell'operatore o ∈ O
+        :param Lo: Lista di richieste assegnate, inizialmente vuota se non specificata.
         """
         self.id = id
         self.home = home
-        self.work_time = work_time
+        self.ho = ho
         
         # Se non viene specificato un current_patient, l'operatore inizia dal suo depot (home)
         self.current_patient = current_patient if current_patient is not None else home
-        self.t0 = t0
+        self.eo = eo
         self.cluster_id = cluster_id
+        self.wo = wo
+        self.Lo = Lo if Lo is not None else []
 
     def __repr__(self) -> str:
-        return (f"Operator(id={self.id}, home={self.home}, work_time={self.work_time:.0f}, "
-                f"current_patient={self.current_patient}, t0={self.t0}, cluster_id={self.cluster_id})")
+        return (f"Operator(id={self.id}, home={self.home}, ho={self.ho:.0f}, "
+                f"current_patient={self.current_patient}, eo={self.eo}, cluster_id={self.cluster_id}, wo={self.wo}, Lo={self.Lo})")
 
 
 class Request:
