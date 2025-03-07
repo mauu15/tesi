b01426cf95d47d517f6fb6d66fe0af98e84b6303
Maurizio <maurizio.mazza@edu.unifi.it>
2025-03-07 18:54:26 +0100

    Enhance data loader: add name and surname fields, adjust max weekly hours calculation, and refine request parameters

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -17,8 +17,10 @@ def read_operators(file_path):
         for row in reader:
             op = {
                 "id": row["id"],
-                "max_weekly_hours": int(float(row["max_weekly_hours"])) if row["max_weekly_hours"] else 0,
-                "available_days": row["available_days"],  # eventuale conversione a lista se necessario
+                "name": row["name"],
+                "surname": row["surname"],
+                "weekly_worked_hours": 0,
+                "max_weekly_hours": int(float(row["max_weekly_hours"]) * 60) if row["max_weekly_hours"] else 0,
                 "lat": float(row["lat"]) if row["lat"] else 0.0,
                 "lon": float(row["lon"]) if row["lon"] else 0.0
             }
@@ -44,8 +46,8 @@ def read_requests(file_path):
             req = {
                 "id": row["id"],
                 "project_id": row["project_id"],
-                "day": int(row["day"]) if row["day"] else 0,
-                "n_operators_required": int(row["n_operators_required"]) if row["n_operators_required"] else 1,
+                "day": int(row["day"]) if row["day"] else None,
+                "n_operators_required": int(row["n_operators_required"]) if row["n_operators_required"] else float("inf"),
                 "duration": int(float(row["duration"])) if row["duration"] else 0,
                 "min_time_begin": int(float(row["min_time_begin"])) if row["min_time_begin"] else 0,
                 "max_time_begin": int(float(row["max_time_begin"])) if row["max_time_begin"] else 0
