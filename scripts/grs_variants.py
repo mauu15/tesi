f2f5401015d752580168ae730623f2fdea5d74dc
Maurizio <maurizio.mazza@edu.unifi.it>
2025-03-23 18:15:48 +0100

    Enhance grs_variants function to return detailed cost metrics and track unused operators

--- scripts/grs_variants.py
+++ scripts/grs_variants.py
@@ -7,6 +7,8 @@ from typing import List, Optional, Tuple
 
 # TOLTA MOMENTANEAMENTE LA SELEZIONE DELLA VARIANTE, DA RIAGGIUNGERE IN INPUT E AGGIUNGERE LE ALTRE VARIANTI E LOGICA DI SELEZIONE  
 def grs_variants(operators, requests, patients, shift_end, down_time_true, tau, k):
+
+   
     # print("[DEBUG] Inizio grs_variants: tau =", tau, type(tau))
     """
     Funzione unica per l'algoritmo GRS che, in base alla variante scelta,
@@ -23,9 +25,16 @@ def grs_variants(operators, requests, patients, shift_end, down_time_true, tau, 
     :param requests: Lista di richieste (dizionari).
     :param patients: cluster di pazienti (dizionari).
     :param shift_end: Orario di fine turno (in minuti).
-    :return: routing_cost e overtime_cost.
+    :return: Tuple con:
+    - feasible: True se la soluzione trovata è ammissibile, False altrimenti.
+    - total_routing_cost: Costo totale degli spostamenti.
+    - total_overtime_cost: Costo totale degli straordinari.
+    - total_waiting_time: Tempo totale di attesa.
+    - not_used_ops: Lista degli operatori non utilizzati.
     """
 
+    used_ops = []
+    feasible = True
     assignments = {}
     total_routing_cost = 0
     total_overtime_cost = 0
@@ -59,7 +68,13 @@ def grs_variants(operators, requests, patients, shift_end, down_time_true, tau, 
 
         if len(feasible_ops) == 0:
             print(f"Richiesta {req['id']} non assegnata: nessun operatore disponibile.")
+            print("Beta_i: ", beta_i, " Alpha_i: ", alpha_i, " Duration: ", req["duration"])
+            print(""*5)
+            # for op in sorted_operators:
+            #     print(f"Operatore {op['id']}: eo = {op['eo']}, current_patient_id = {op['current_patient_id']}, ho = {op['ho']} tau = {tau[op['current_patient_id'], req['project_id']]}, waiting_time = {waiting_time[op['id']]}")
 
+            feasible = False
+           
 
         else:
             for op in feasible_ops:
@@ -89,6 +104,9 @@ def grs_variants(operators, requests, patients, shift_end, down_time_true, tau, 
         if best_op is not None:
             travel_time = tau[best_op["current_patient_id"], req["project_id"]]
 
+
+            # print("Request ", req["id"], " assigned to operator ", best_op["id"], " with f_oi = ", best_f_oi, " and waiting time = ", waiting_time[best_op["id"]])
+            # input()
 
             # Calcolo dei tempi:
             # arrival_time: l'operatore arriva al paziente
@@ -122,6 +140,9 @@ def grs_variants(operators, requests, patients, shift_end, down_time_true, tau, 
             best_op["Lo_k"][k].append((req, b_i)) # Lo = Lo U (i,b_i)
             # [DEBUG] print(f"[DEBUG grs_variants] Operatore {best_op['id']} global_assignments: {best_op['global_assignments']}")
 
+            # segno quanto overtime ha fatto l'operatore
+            best_op["overtime_minutes_k"][k] = max(best_op["wo_k"][k] - best_op["Ho"], 0)
+
             req["b_i"] = b_i
 
             # se b_i, inizio del servizio, è dopo le 11:30 e la richiesta non può iniziare dopo le 12:30
@@ -129,6 +150,8 @@ def grs_variants(operators, requests, patients, shift_end, down_time_true, tau, 
             # if b_i >= 11*60 + 30 and req["min_time_begin"] < 12*60 + 30:
             #     #Aggiungo che best op ha lavorato turno pieno la mattina e in questo giorno non può fare doppio turno
             #     best_op["worked_after_11:30am"] = True
+
+            used_ops.append(best_op["id"])
 
             if b_i >= 11*60 + 30 and alpha_i < 12*60 + 30:
                 best_op["worked_after_11:30am_k"][k] = True
@@ -138,9 +161,11 @@ def grs_variants(operators, requests, patients, shift_end, down_time_true, tau, 
             # Aggiorno il costo totale
             total_routing_cost += best_r_c
             total_overtime_cost += best_ov_c
-        
+    
+    operators_id = [op["id"] for op in operators]
+    not_used_ops = [op for op in operators_id if op not in used_ops]
 
-    return total_routing_cost, total_overtime_cost, sum(op["do_k"][k] for op in operators)
+    return feasible, total_routing_cost, total_overtime_cost, sum(op["do_k"][k] for op in operators), not_used_ops
 
 ###############################################################################
 # Funzione per calcolare il costo extra (f_oi) dell'assegnazione
@@ -179,9 +204,9 @@ def compute_f_oi(operator, request, waiting_time, theta=0.37, tau=None, down_tim
     if operator["wo"] + service_time + travel_time + waiting_time > operator["Ho"]:
         op_cost_per_minute = 0.29 # C_o, 17.5 €/h, quindi 0.29 €/min
         
-        overtime_penalty = op_cost_per_minute * (service_time + travel_time + waiting_time+min(operator["wo"] - operator["Ho"], 0))
+        overtime_cost = op_cost_per_minute * (service_time + travel_time + waiting_time+min(operator["wo"] - operator["Ho"], 0))
     else:
-        overtime_penalty = 0
+        overtime_cost = 0
 
     if down_time_true:
         d_t_t = 1
@@ -189,8 +214,8 @@ def compute_f_oi(operator, request, waiting_time, theta=0.37, tau=None, down_tim
         d_t_t = 0
 
     routing_cost = theta * travel_time
-    overtime_cost = overtime_penalty
     waiting_cost = ((theta**2) * waiting_time) * d_t_t
+    #waiting_cost = 100*waiting_time * d_t_t
 
 
 
